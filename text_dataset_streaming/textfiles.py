6a4b069c64a210090b37636c7f4e2cc32cb836f8
cedspam <cedric.lacrambe@gmail.com>
2019-10-23 16:09:41 +0200

    Update textfiles.py

--- text_dataset_streaming/textfiles.py
+++ text_dataset_streaming/textfiles.py
@@ -16,10 +16,18 @@ def urllist_textgen(urls,chunk_size=int(32e6),encoding="utf8"):
         except:
             logging.exception("eception url %s",u)
 
-def opus_mono_textgen(lang='fr',encoding='utf8',chunk_size=int(32e6),minsize=5e3):
-    r=requests.get("http://opus.nlpl.eu/opusapi/",params={"source":lang,"version":"latest", 'preprocessing': 'mono'})
+def opus_mono_get_url(lang='fr',minsize=5e3):
+    r=requests.get("http://opus.nlpl.eu/opusapi/",
+                   params={"source":lang,
+                                                  "version":"latest",
+                                                  'preprocessing': 'mono'})
     d=r.json()
     urls=sorted(set( c['url']   for c in d['corpora'] \
             if  '.txt.gz' in c['url'] and int(c['size'])>minsize ))
-    random.shuffle(urls)
+    return urls
+
+
+def opus_mono_textgen(lang='fr',encoding='utf8',chunk_size=int(8e6),minsize=5e3):
+    urls=opus_mono_get_url(lang,minsize)
     return  urllist_textgen(urls,chunk_size,encoding)
+

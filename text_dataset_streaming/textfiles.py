9b3ebcaad8cadc5348b990fd9447b18a2bdca597
LACRAMBE Cédric <cedric.lacrambe@gmail.com>
2019-10-28 14:59:33 +0100

    split_ligne

--- text_dataset_streaming/textfiles.py
+++ text_dataset_streaming/textfiles.py
@@ -4,17 +4,43 @@ import smart_open
 import random
 import logging
 
+def split_ligne(t,chunk_size=int(32e6)):
+    lreste=chunk_size-len(t)
+    lreste=int(lreste)
+    if lreste<0:
+        return t,""
+    else:
+        l=t[4096:].rsplit("\n",1)
+        if len(l)>1:
+              texte1,reste=l
+        else:
+              texte=l[0]
+              reste=""
+        texte=t[:4096]+texte
+        return texte,reste
+
+
 def urllist_textgen(urls,chunk_size=int(32e6),encoding="utf8"):
     for u in urls:
+        reste=""
         try:
             with smart_open.open(u,
                      encoding=encoding,errors="ignore") as f:
               t=f.read(chunk_size)
+              texte,reste=split_ligne(t,chunk_size*0.8)
+
+
+
+
+
               while len(t)>0:
-                yield t
+                yield texte
                 t=f.read(chunk_size)
+                texte,reste=split_ligne(reste+t,chunk_size)
+            if len(reste)>0:
+                yield reste
         except:
-            logging.exception("eception url %s",u)
+            logging.exception("exception url %s",u)
 
 def opus_mono_get_url(lang='fr',minsize=5e3):
     r=requests.get("http://opus.nlpl.eu/opusapi/",

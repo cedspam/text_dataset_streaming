060f72580e199f331e3261ee5422c45df6be673d
cedspam <cedric.lacrambe@gmail.com>
2019-10-28 17:23:55 +0100

    Update textfiles.py

--- text_dataset_streaming/textfiles.py
+++ text_dataset_streaming/textfiles.py
@@ -46,7 +46,7 @@ def url_textgen(u,chunk_size=int(32e6),encoding="utf8"):
 def urllist_to_textgen_list(urls,chunk_size=int(32e6),encoding="utf8"):
     textgen_func=functools.partial(url_textgen,chunk_size=chunk_size,
                                    encoding=encoding)
-    return map(textgen_func,textgen_func)
+    return map(textgen_func,urls)
 
 def urllist_textgen(urls,chunk_size=int(32e6),encoding="utf8"):
     iters=urllist_to_textgen_list(urls,chunk_size=chunk_size,

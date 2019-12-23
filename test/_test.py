b291df9d20203e7d7f6477fa9d26f159bf1da290
cedspam <cedric.lacrambe@gmail.com>
2019-12-23 11:37:00 +0100

    Update _test.py

--- test/_test.py
+++ test/_test.py
@@ -5,9 +5,10 @@ from text_dataset_streaming import wiki_article_generator
 
 def test_opus():
     gen=opus_mono_textgen(encoding='utf8',chunk_size=16000)
-    text=next(gen)
-
-    assert abs(len(text)-16000)<300
+    liste_textes=[next(gen) for _ in range(20)]
+    
+    assert all(len(t)>10 for t in liste_textes)
+    
 
 
 
